56a174d8d74bcdc69f41e5704a70d00a40fc1cd3
Narek <narek.hakobyan.07@gmail.com>
2021-12-31 02:27:06 +0400

    initial commit

---
+++ components/Statistic.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import { ref } from "vue";
+
+const props = defineProps({
+  count: {
+    type: String,
+    required: true,
+  },
+  text: {
+    type: String,
+    required: true,
+  },
+});
+
+const count = ref(props.count);
+const text = ref(props.text);
+</script>
+
+<template>
+  <div flex="~ col auto" align="items-center">
+      <b text="5xl" m="2">{{ count }}</b>
+      <b>{{ text }}</b>
+    </div>
+
+    
+</template>
